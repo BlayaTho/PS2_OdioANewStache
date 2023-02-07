f8f17f76f5bb195d07e1f54e098613e704ba2e73
T h  o m a s <blayathomas1408@gmail.com>
2023-02-07 19:41:27 +0100

    V1 Proto dash propulsion

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,15 +8,19 @@ public class Player : MonoBehaviour
     SpriteRenderer sr;
     Animator animController;
     float horizontal_value;
+    float vertical_value;
     Vector2 ref_velocity = Vector2.zero;
 
-    float jumpForce = 12f;
-
+    [SerializeField] float jumpForce = 12f;
     [SerializeField] float moveSpeed_horizontal = 400.0f;
+    [SerializeField] float moveSpeed_vertical = 0f;
     [SerializeField] bool is_jumping = false;
+    [SerializeField] bool is_dashing = false;
+    [SerializeField] bool aerial = false;
     [SerializeField] bool can_jump = false;
     [Range(0, 1)][SerializeField] float smooth_time = 0.5f;
 
+    
 
     // Start is called before the first frame update
     void Start()
@@ -24,15 +28,16 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         animController = GetComponent<Animator>();
-        //Debug.Log(Mathf.Lerp(current, target, 0));
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal_value = Input.GetAxis("Horizontal");
+        vertical_value = Input.GetAxis("Vertical");
 
-        if(horizontal_value > 0) sr.flipX = false;
+        if (horizontal_value > 0) sr.flipX = false;
         else if (horizontal_value < 0) sr.flipX = true;
         
         animController.SetFloat("Speed", Mathf.Abs(horizontal_value));
@@ -42,26 +47,65 @@ public class Player : MonoBehaviour
             is_jumping = true;
             animController.SetBool("Jumping", true);
         }
+
+        // Appuyer sur la touche ctrl gauche pour lancer la co routine si le personnage ne dahs pas deja et est en l'air
+        if (Input.GetButtonDown("Fire1") && is_dashing == false && aerial )
+        {
+            StartCoroutine(DashPropulsion());
+        }
     }
     void FixedUpdate()
     {
         if (is_jumping && can_jump)
         {           
             is_jumping = false;
+            aerial = true;
+            
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             can_jump = false;
         }
-        Vector2 target_velocity = new Vector2(horizontal_value * moveSpeed_horizontal * Time.fixedDeltaTime, rb.velocity.y);
-        rb.velocity = Vector2.SmoothDamp(rb.velocity, target_velocity, ref ref_velocity, 0.05f);
-        
+        // flip flop pour couper le rb velocity du premier vector qui bloque les deplacements sur le y lors du dash
+        if (is_dashing == false)
+        {
+            Vector2 target_velocity = new Vector2(horizontal_value * moveSpeed_horizontal * Time.fixedDeltaTime, rb.velocity.y);
+            rb.velocity = Vector2.SmoothDamp(rb.velocity, target_velocity, ref ref_velocity, 0.05f);
+        }
+        else if (is_dashing)
+        {
+            Vector2 target_velocitydash = new Vector2(horizontal_value * moveSpeed_horizontal * Time.fixedDeltaTime, vertical_value * moveSpeed_vertical * Time.fixedDeltaTime);
+            rb.velocity = Vector2.SmoothDamp(rb.velocity, target_velocitydash, ref ref_velocity, 0.05f);
+        }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
         can_jump = true;
+        aerial = false;
+        rb.gravityScale = 3f;
+        moveSpeed_horizontal = 400f;
         animController.SetBool("Jumping", false);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {   
         animController.SetBool("Jumping", false);        
     }
+
+    // Co routine pour modifier les variables necessaires, couper l'anim de jump et changer la gravité et les controles pendant 1.2s puis remettre ce qu'il faut
+    IEnumerator DashPropulsion()
+    {
+        is_dashing = true;
+        animController.SetBool("Aerial", true);
+        animController.SetBool("Jumping", false);
+        rb.gravityScale = 0;
+        moveSpeed_vertical = 500f;
+        moveSpeed_vertical = 500f;
+
+        yield return new WaitForSeconds(1.2f);
+        is_dashing = false;
+        animController.SetBool("Aerial", false);
+        moveSpeed_vertical = 0f;
+        moveSpeed_vertical = 400f;
+        moveSpeed_horizontal = 550f;
+        rb.gravityScale = 5.5f;
+    }
+
 }
