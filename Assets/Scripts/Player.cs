45e884ab6477686b5c91217473ce0a667ef2d850
T h  o m a s <blayathomas1408@gmail.com>
2023-02-12 21:54:53 +0100

    Implémentation Dash et doubleJump
    Amélioration Gamefeel (modif valeurs), rangement et explication du code, implémentation Dash et double jump avec bug avec le dash + Propulsion

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,29 +7,49 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
     SpriteRenderer sr;
     Animator animController;
-    float horizontal_value;
-    float vertical_value;
+    [SerializeField] float horizontal_value;
+    [SerializeField] float vertical_value;
     Vector2 ref_velocity = Vector2.zero;
 
-    [SerializeField] float jumpForce = 12f;
-    [SerializeField] float moveSpeed_horizontal = 400.0f;
+    //DEPLACEMENT 
+    [SerializeField] float jumpForce ;
+    [SerializeField] float moveSpeed_horizontal ;
     [SerializeField] float moveSpeed_vertical = 0f;
     [SerializeField] bool is_jumping = false;
+    [SerializeField] bool can_jump = false;
+    [Range(0, 1)][SerializeField] float smooth_time ;
+   
+
+    //DASH PROPULSION
     [SerializeField] bool is_dashing = false;
     [SerializeField] bool aerial = false;
-    [SerializeField] bool can_jump = false;
-    [Range(0, 1)][SerializeField] float smooth_time = 0.5f;
-    [Range(0, 15)][SerializeField] float gravityfall;
+
+    float Timerseconde = 1.2f;
 
     [SerializeField] float smoothdash;
-
     [SerializeField] float speedverti;
     [SerializeField] float speedhori;
+   
+    [Range(0, 15)][SerializeField] float gravityfall;
 
-    [SerializeField] int fallGravityMultiplier;
+
+    //DOUBLE JUMP
+    [SerializeField] bool IsGrounded = false;
+    [SerializeField] int CountJump = 2;
+    private int LastPressedJumpTime = 0;
+    private int LastOnGroundTime = 0;
+    [SerializeField] float jumpForceAerial;
 
 
-    // Start is called before the first frame update
+    //DASH
+    private bool canDash = true;
+    private bool isDashing;
+    [SerializeField] private float dashingPower;
+    [SerializeField] private float dashingTime ;
+    [SerializeField] private float dashingCooldown ;
+    [SerializeField] private TrailRenderer tr;
+    
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -38,29 +58,70 @@ public class Player : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         horizontal_value = Input.GetAxis("Horizontal");
         vertical_value = Input.GetAxis("Vertical");
 
+        // Flip Du Sprite selon la direction
         if (horizontal_value > 0) sr.flipX = false;
         else if (horizontal_value < 0) sr.flipX = true;
+
+
+        if (isDashing)
+        {
+            return ;
+        }
         
         animController.SetFloat("Speed", Mathf.Abs(horizontal_value));
    
+        //Jump = à la touche A et Y
         if (Input.GetButtonDown("Jump") && can_jump)
         {
             is_jumping = true;
             animController.SetBool("Jumping", true);
         }
 
-        // Appuyer sur la touche ctrl gauche pour lancer la co routine si le personnage ne dahs pas deja et est en l'air
-        if (Input.GetButtonDown("Fire1") && is_dashing == false && aerial )
+        if (Input.GetButtonDown("Jump") && aerial && CountJump > 0)
+        {
+            PhysicDoubleJump();
+
+        }
+
+      
+        // Fire1 = à la touche X
+        if (Input.GetButton("Fire1") && is_dashing == false && aerial)
         {
             StartCoroutine(DashPropulsion());
+
+        }
+        
+        // Fire 2 = a la touche B
+        if (Input.GetButtonDown("Fire2") && canDash)
+        {
+            StartCoroutine(Dash());
         }
     }
+
+    void PhysicDoubleJump()
+    {
+        // Garantit que nous ne pouvons pas appeler Jump plusieurs fois à partir d'une seule pression
+        LastPressedJumpTime = 0;
+        LastOnGroundTime = 0;
+        CountJump -= 1;
+
+        // On augmente la force appliquée si on tombe
+        // Cela signifie que nous aurons toujours l'impression de sauter le même montant
+        float force = jumpForceAerial;
+        if (rb.velocity.y < 0)
+            force -= rb.velocity.y;
+
+
+        rb.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+
+    }
+
+
     void FixedUpdate()
     {
         if (is_jumping && can_jump)
@@ -71,11 +132,17 @@ public class Player : MonoBehaviour
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             can_jump = false;
         }
+
+        if (isDashing)
+        {
+            return;
+        }
+        
         // flip flop pour couper le rb velocity du premier vector qui bloque les deplacements sur le y lors du dash
         if (is_dashing == false)
         {
             Vector2 target_velocity = new Vector2(horizontal_value * moveSpeed_horizontal * Time.fixedDeltaTime, rb.velocity.y);
-            rb.velocity = Vector2.SmoothDamp(rb.velocity, target_velocity, ref ref_velocity, 0.05f);
+            rb.velocity = Vector2.SmoothDamp(rb.velocity, target_velocity, ref ref_velocity, smooth_time);
         }
         else if (is_dashing)
         {
@@ -83,29 +150,32 @@ public class Player : MonoBehaviour
             rb.velocity = Vector2.SmoothDamp(rb.velocity, target_velocitydash, ref ref_velocity, smoothdash);
             
         }
-       /* #region
-        if (rb.velocity.y < 0)
-        {
-            rb.gravityScale = gravityfall * fallGravityMultiplier;
-        }
-        else
-        {
-            rb.gravityScale = gravityfall;
-        }
-        #endregion */
+
+
+        /* if (horizontal_value == 0)
+         {
+             Timerseconde = 0;
+         }
+         */
+         
+        
+        
     }
+
+    // Lorsque le personnage touche le sol
     private void OnTriggerStay2D(Collider2D collision)
     {
         can_jump = true;
         aerial = false;
-        rb.gravityScale = 3f;
-        moveSpeed_horizontal = 400f;
+        rb.gravityScale = 4f;
+        moveSpeed_horizontal = 500f;
         animController.SetBool("Jumping", false);
+        CountJump = 2; //reset double saut quand on touche le sol
+        
+        Timerseconde = 1.2f;
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {   
-        animController.SetBool("Jumping", false);        
-    }
+    
+ 
 
     // Co routine pour modifier les variables necessaires, couper l'anim de jump et changer la gravité et les controles pendant 1.2s puis remettre ce qu'il faut
     IEnumerator DashPropulsion()
@@ -117,13 +187,35 @@ public class Player : MonoBehaviour
         moveSpeed_vertical = speedverti;
         moveSpeed_horizontal = speedhori;
 
-        yield return new WaitForSeconds(1.2f);
+            yield return new WaitForSeconds(Timerseconde);
+   
         is_dashing = false;
         animController.SetBool("Aerial", false);
-        moveSpeed_vertical = 0f;
+        
         moveSpeed_vertical = 400f;
         moveSpeed_horizontal = 550f;
         rb.gravityScale = gravityfall;
     }
 
+    IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        if (sr.flipX == true)
+        {
+            rb.velocity = new Vector2(-transform.localScale.x * dashingPower, 0f);
+        }
+        else
+        { rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f); }
+
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    }
 }
